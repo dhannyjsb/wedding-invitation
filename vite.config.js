67a8e73115b4ad759f100ee5293a7139e83532d0
Oka R. Abdillah <hans.abdillh05@gmail.com>
2022-05-04 21:02:47 +0800

    install vite-svg

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
+import SVGLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), SVGLoader()],
   define: {
     'process.env': {}
   },
