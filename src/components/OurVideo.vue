f1b266c12c2956b85b170dae84f6c4cde21d90dd
Oka R. Abdillah <hans.abdillh05@gmail.com>
2022-05-06 23:56:35 +0800

    Install vue3-player-video

--- src/components/OurVideo.vue
+++ src/components/OurVideo.vue
@@ -1,11 +1,13 @@
 <template>
   <section class="container-section bg-slate-100">
     <HeaderSection title="Our Video" /> 
+    <VueVideoPlayer data-aos="zoom-in" class="w-10/12 mx-auto" src="https://dl.dropbox.com/s/ad8kglphakmuacv/happy_eid_mubarak_atiya.mp4?dl=2" />
   </section>
 </template>
 
 <script setup>
 
+import VueVideoPlayer from 'vue3-player-video'
 import HeaderSection from '@/components/HeaderSection.vue'
 
 </script>
