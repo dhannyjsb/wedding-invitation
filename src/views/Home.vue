8b54cb98c71c47e1ddb34e006e3ecf88c48a04f4
Oka R. Abdillah <67676285+or-abdillh@users.noreply.github.com>
2022-05-14 17:10:21 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,20 @@
 <template>
   <section>
     <Wellcome></Wellcome>
-    <MainCover id="mainCover"></MainCover>
-    <Information></Information>
-    <GroomBride id="groomAndBride"></GroomBride>
-    <GoToOurWedding id="goToOurWedding"></GoToOurWedding>
-    <OurGallery id="ourGallery"></OurGallery>
-    <OurStory></OurStory>
-    <GuestBook id="guestBook"></GuestBook>
+    <MainCover v-if="isAudioPlay" id="mainCover"></MainCover>
+    <Information v-if="isAudioPlay"></Information>
+    <GroomBride v-if="isAudioPlay" id="groomAndBride"></GroomBride>
+    <GoToOurWedding v-if="isAudioPlay" id="goToOurWedding"></GoToOurWedding>
+    <OurGallery v-if="isAudioPlay" id="ourGallery"></OurGallery>
+    <OurStory v-if="isAudioPlay"></OurStory>
+    <GuestBook v-if="isAudioPlay" id="guestBook"></GuestBook>
   </section>
-  <Navigation v-on:navChange="navigationHandler"></Navigation>
+  <Navigation v-if="isAudioPlay" v-on:navChange="navigationHandler"></Navigation>
 </template>
 
 <script setup>
-
+import { useState } from '@/stores/state.js'
+import { computed } from 'vue'
 import Wellcome from '@/components/Wellcome.vue'
 import MainCover from '@/components/MainCover.vue'
 import Information from '@/components/Information.vue'
@@ -23,6 +24,9 @@ import OurGallery from '@/components/OurGallery.vue'
 import OurStory from '@/components/OurStory.vue'
 import GuestBook from '@/components/GuestBook.vue'
 import Navigation from '@/components/Navigation.vue'
+
+const state = useState()
+const isAudioPlay = computed(() => state.isAudioPlay)
 
 // Navigation handler
 const navigationHandler = id => {
