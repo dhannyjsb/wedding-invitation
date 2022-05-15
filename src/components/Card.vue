34920f856bbf9a249a509e384731e441182862c3
Oka R. Abdillah <hans.abdillh05@gmail.com>
2022-05-15 15:38:15 +0800

    Update Card UI component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <section data-aos="zoom-in" :class="props.bgColor" class="w-9/12 mx-auto rounded-tl-[2rem] rounded-br-[2rem] shadow-xl overflow-hidden">
+  <section data-aos="zoom-in" :class="props.bgColor" class="w-9/12 mx-auto rounded-tl-[2rem] rounded-br-[2rem] shadow-xl overflow-hidden border border-gray-200">
     <div data-aos="fade-up"><slot name="body"></slot></div>
     <div data-aos="fade-up"><slot name="footer"></slot></div>
   </section>
