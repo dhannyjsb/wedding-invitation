fd61b6a9793198878bf7c7668ab3dd72a8d98fb8
Oka R. Abdillah <hans.abdillh05@gmail.com>
2022-05-16 19:20:34 +0800

    Update UI our story

--- src/components/OurStory.vue
+++ src/components/OurStory.vue
@@ -3,15 +3,21 @@
     <section class="container-section">
       <HeaderSection title="Kisah Cinta Kami" />
       <div class="mt-6">
-        <template v-for="(story, x) in stories" :key="x">
-          <div 
-            data-aos="zoom-in"
-            class="w-10/12 mx-auto mb-4">
-            <p class="satisfy-font text-3xl text-amber-500 text-xl">{{ story.title }}</p>
-            <p class="font-medium mb-0">{{ story.place }}</p>
-            <p>{{ story.description }}</p>
-          </div>
-        </template>
+        <Card>
+          <template v-slot:body>
+            <template v-for="(story, x) in stories" :key="x">
+              <div 
+                data-aos="zoom-in"
+                class="w-10/12 text-center text-sm p-4 mx-auto mb-4">
+                <decoration class="w-2/12 mx-auto fill-gray-800"></decoration>
+                <p class="satisfy-font text-3xl text-amber-500 text-xl">{{ story.title }}</p>
+                <p class="font-medium mb-0">{{ story.place }}</p>
+                <p class="text-justify">{{ story.description }}</p>
+              </div>
+            </template>
+          </template>
+          <template v-slot:footer><img src="@/assets/images/couples.png" alt=""></template>
+        </Card>
       </div>
     </section>
   </section>
@@ -22,7 +28,8 @@
 import axios from 'axios'
 import { ref, onMounted } from 'vue'
 import HeaderSection from '@/components/HeaderSection.vue'
-import wave from '@/assets/svg/wave.svg'
+import Card from '@/components/Card.vue'
+import decoration from '@/assets/svg/decoration-2.svg'
 
 const stories = ref(null)
 onMounted(() => {
