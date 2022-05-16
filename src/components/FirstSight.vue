0d4b3c4d5d67b416e6ae3bb4b4a5bcd6bc9d36a9
Oka R. Abdillah <hans.abdillh05@gmail.com>
2022-05-16 19:22:31 +0800

    Update UI first sight

--- src/components/FirstSight.vue
+++ src/components/FirstSight.vue
@@ -5,7 +5,7 @@
       <HeaderSection title="Pandangan Pertama"></HeaderSection>
       <Card title="Kisah kami berawal dari">
         <template v-slot:body>
-          <p class="text-sm p-6">
+          <p class="text-sm p-6 text-justify">
             In faucibus orci luctus et ultrices posuere cubilia Curae; Vestibulum hendrerit malesuada odio. Fusce ut elit ut augue sollicitudin blandit. Phasellus volutpat lorem. Duis non pede et neque luctus tincidunt. Duis interdum tempus elit. Aenean metus. Vestibulum ac lacus. Vivamus porttitor, massa ut hendrerit bibendum, metus.
             <br><br>
             Nunc et sem. Aliquam tempor. Nam lobortis sem non urna. Pellentesque et urna sit amet leo accumsan volutpat. Nam molestie lobortis.
